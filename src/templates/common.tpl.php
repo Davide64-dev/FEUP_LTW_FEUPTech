60d2b20f0b7edb97f9b9d6a33bc149484235767e
Davide Teixeira <davidpcu@hotmail.com>
2023-04-13 17:15:24 +0100

    started php

---
+++ src/templates/common.tpl.php
@@ -0,0 +1,23 @@
+<?php function drawHeader() { ?>
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Music Shop</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../profile.css">
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <script src="https://kit.fontawesome.com/38229b6c34.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+
+    <header>
+      <h1>FEUPTech</h1>
+    </header>
+  
+    <section id="messages">
+        <p>Hello World!</p>
+    </section>
+
+    <main>
+<?php } ?>
